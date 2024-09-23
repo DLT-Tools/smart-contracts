baa0f4b7eae04e07abb6061d609e61f139ecb49f
Anton P. <anton@antonp.com>
2024-09-23 11:06:10 +0200

    v1.0.9
    Added more functions on library DLTBlacklist

--- contracts/lib/DLTBlacklist.sol
+++ contracts/lib/DLTBlacklist.sol
@@ -3,14 +3,37 @@ pragma solidity 0.8.27;
 
 import { IDLTConfig } from '../interfaces/publicDir/IDLTConfig.sol';
 import { IDLTBlacklistStorage } from '../interfaces/IDLTBlacklistStorage.sol';
+import { BlockedNFT } from '../models/BlockedNFT.sol';
+import { BlockedWallet } from '../models/BlockedWallet.sol';
 
 library DLTBlacklist {
+    // Custom errors
+    error WalletIsBlocked(address wallet, string reasonBlocking);
+    error NFTIsBlocked(address collection, uint256 tokenId, string reasonBlocking);
+
+    // Methods
+    function checkNotBlockedWallet(address dltConfig_, address wallet_) public view {
+        address dltBlacklistStorageAddr = IDLTConfig(dltConfig_).getDLTBlacklistStorage();
+        BlockedWallet memory blockedWallet = IDLTBlacklistStorage(dltBlacklistStorageAddr).getBlockedWallet(wallet_);
+        if (blockedWallet.isBlocked) {
+            revert WalletIsBlocked(wallet_, blockedWallet.reasonBlocking);
+        }
+    }
+
+    function checkNotBlockedNFT(address dltConfig_, address collection_, uint256 tokenId_) public view {
+        address dltBlacklistStorageAddr = IDLTConfig(dltConfig_).getDLTBlacklistStorage();
+        BlockedNFT memory blockedNFT = IDLTBlacklistStorage(dltBlacklistStorageAddr).getBlockedNFT(collection_, tokenId_);
+        if (blockedNFT.isBlocked) {
+            revert NFTIsBlocked(collection_, tokenId_, blockedNFT.reasonBlocking);
+        }
+    }
+
     function isBlockedWallet(address dltConfig_, address wallet_) public view returns (bool) {
         address dltBlacklistStorageAddr = IDLTConfig(dltConfig_).getDLTBlacklistStorage();
         return IDLTBlacklistStorage(dltBlacklistStorageAddr).getBlockedWallet(wallet_).isBlocked;
     }
 
-    function isBlockedWallet(address dltConfig_, address collection_, uint256 tokenId_) public view returns (bool) {
+    function isBlockedNFT(address dltConfig_, address collection_, uint256 tokenId_) public view returns (bool) {
         address dltBlacklistStorageAddr = IDLTConfig(dltConfig_).getDLTBlacklistStorage();
         return IDLTBlacklistStorage(dltBlacklistStorageAddr).getBlockedNFT(collection_, tokenId_).isBlocked;
     }
